9892887ad6e1a46ffe2b5f50ec1c2cf573a3acca
Charlie Fenoglio <jcfenoglio@gmail.com>
2023-07-05 20:08:22 -0700

    Tuned CSS to not hurt my eyes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,131 +1,83 @@
 import './App.css';
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ProfSlider from './components/ProfSlider';
+import Specs from './data/Specs.json';
+import Crafts from './data/Crafts.json';
 
-const lwSpecs = []
-const jcSpecs = []
-const alchSpecs = []
-const bsSpecs = []
-const enchSpecs = []
-const inscSpecs = []
-const tailorSpecs = []
+function App(props) {
+  // this.state = {
+  //   currentProf: "lw",
+  //   knowList: lwSpecs,
+  //   knowledge: new Map(),
+  // };
+  const [currentProf, setCurrentProf] = useState("lw");
+  const [knowledge, setKnowledge] = useState(new Map());
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentProf: "lw",
-      knowList: lwSpecs,
-      knowledge: new Map(),
+  const tabSwitch = (prof) => {
+      setCurrentProf(prof);
     };
-  }
 
-  handleSliderChange = (specName, newValue) => {
-    this.setState({ knowledge: new Map(this.state.knowledge.set(specName, newValue)) });
-  }
-
-  tabSwitch = (prof) => {
-    switch(prof) {
-      case "jc":
-        return this.setState({ currentProf: "jc", knowList: jcSpecs });
-      case "alch":
-        return this.setState({ currentProf: "alch", knowList: alchSpecs});
-      case "bs":
-        return this.setState({ currentProf: "bs", knowList: bsSpecs });
-      case "ench":
-        return this.setState({ currentProf: "ench", knowList: enchSpecs });
-      case "insc":
-        return this.setState({ currentProf: "insc", knowList: inscSpecs });
-      case "tailor":
-        return this.setState({ currentProf: "tailor", knowList: tailorSpecs });
-      default:
-        return this.setState({ currentProf: "lw", knowList: lwSpecs });
-    }
-  }
-
-  renderTabList = () => {
+  const renderTabList = () => {
     return (
       <div className="nav nav-tabs">
-        <span
-          className={this.state.currentProf === "lw" ? "nav-link active" : "nav-link"}
-          onClick={() => this.tabSwitch("lw")}
-        >
-          Leatherworking
-        </span>
-        <span
-          className={this.state.currentProf === "bs" ? "nav-link active" : "nav-link"}
-          onClick={() => this.tabSwitch("bs")}
-        >
-          Blacksmithing
-        </span>
-        <span
-          className={this.state.currentProf === "tailor" ? "nav-link active" : "nav-link"}
-          onClick={() => this.tabSwitch("tailor")}
-        >
-          Tailoring
-        </span>
-        <span
-          className={this.state.currentProf === "jc" ? "nav-link active" : "nav-link"}
-          onClick={() => this.tabSwitch("jc")}
-        >
-          Jewelcrafting
-        </span>
-        <span
-          className={this.state.currentProf === "alch" ? "nav-link active" : "nav-link"}
-          onClick={() => this.tabSwitch("alch")}
-        >
-          Alchemy
-        </span>
-        <span
-          className={this.state.currentProf === "ench" ? "nav-link active" : "nav-link"}
-          onClick={() => this.tabSwitch("ench")}
-        >
-          Enchanting
-        </span>
-        <span
-          className={this.state.currentProf === "insc" ? "nav-link active" : "nav-link"}
-          onClick={() => this.tabSwitch("insc")}
-        >
-          Inscription
-        </span>
+        { Specs.map((spec) => 
+          (
+            <span
+              className={currentProf === spec.id ? "nav-link active" : "nav-link"}
+              onClick={() => tabSwitch(spec.id)}
+            >
+              { spec.name }
+            </span>
+          ))}
       </div>
     );
   };
 
-  renderItems = () => {
-    const newSpecs = this.state.knowList;
+  // const renderCrafts = () => {
+  //   let currentCrafts = Crafts.filter((craftList) => {
+  //     return currentProf === craftList.id;
+  //   });
+  //   return (
+  //     <div className=""></div>
+  //   );
+  // }
 
-    return newSpecs.map((spec) => (
-      <li
-        key={spec.name}
-        className="list-group-item flex justify-content-between align-items-center"
-      >
-        <ProfSlider 
-          spec={spec}
-          knowledge={ this.state.knowledge.has(spec.name) ? this.state.knowledge.get(spec.name) : 0 } 
-          updateData={(specName, newValue) => this.handleSliderChange(specName, newValue)}
-        />
-      </li>
-    ));
-  };
+  // const renderItems = () => {
+  //   const newSpecs = this.state.knowList;
 
-  render() {
-    return (
-      <main className="container">
-        <h1 className="text-uppercase text-center my-4">Professions Calculator</h1>
-        <div className="row">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
-              {this.renderTabList()}
-              <ul className="list-group list-group-flush border-top-0">
-                {this.renderItems()}
-              </ul>
-            </div>
+  //   return newSpecs.map((spec) => (
+  //     <li
+  //       key={spec.name}
+  //       className="list-group-item flex justify-content-between align-items-center"
+  //     >
+  //       <ProfSlider 
+  //         spec={spec}
+  //         knowledge={ this.state.knowledge.has(spec.name) ? this.state.knowledge.get(spec.name) : 0 } 
+  //         updateData={(specName, newValue) => this.handleSliderChange(specName, newValue)}
+  //       />
+  //     </li>
+  //   ));
+  // };
+
+  //   const handleSliderChange = (specName, newValue) => {
+  //     this.setState({ knowledge: new Map(this.state.knowledge.set(specName, newValue)) });
+  //   }
+
+  return (
+    <main className="container dark">
+      <h1 className="text-uppercase text-center">Professions Calculator</h1>
+      <div className="row">
+        <div className="mx-auto">
+          <div className="card p-3 dark">
+            {renderTabList()}
+            <ul className="list-group list-group-flush border-top-0 dark">
+              {/* {renderCrafts()} */}
+            </ul>
           </div>
         </div>
-      </main>
-    );
-  }
-}
+      </div>
+    </main>
+  );
+};
 
 export default App;
